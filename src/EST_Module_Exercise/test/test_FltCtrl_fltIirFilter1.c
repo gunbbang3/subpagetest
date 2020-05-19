aee0b597735ce54711f51e6faba66df6981fc533
Wootaik Lee <wootaik@gmail.com>
2020-05-19 22:03:57 +0900

    no message

--- src/EST_Module_Exercise/test/test_FltCtrl_fltIirFilter1.c
+++ src/EST_Module_Exercise/test/test_FltCtrl_fltIirFilter1.c
@@ -10,36 +10,37 @@ void tearDown(void) {
     // clean stuff up here
 }
 
-void test_fltMaFilter(void)
+void test_fltIirFilter1(void)
 {
-	FILE *fp = fopen(".\\test\\data\\FltMaFilterResult.csv","w");
+	FILE *fp = fopen(".\\test\\data\\FltIirFilter1Result.csv","w");
 
 	float_t input;
 	float_t output;
 
-	fltMa_t my_ma = FLT_MA_FILTER_DEFAULT;
+	fltIir_t my_iir = FLT_IIR_FILTER1_DEFAULT;
 
     // filter coefficients
-    my_ma.flt_acc           = (float_t)  0.0 ;
-    my_ma.i16_length        = (int16_t)  16 ;
+    my_iir.flt_b0           = (float_t)  0.1 ;
+    my_iir.flt_b1           = (float_t)  0.0 ;
+    my_iir.flt_a1           = (float_t) -0.9 ;
 
     fprintf(fp, "idx,  input,  output\n");
 
     input  = (float_t) 0.0;
     for (int var = 0; var < 5; ++var) {
-        output = fltMaFilter(input,&my_ma);
+        output = fltIirFilter1(input,&my_iir);
         fprintf(fp,"%d,%f,%f\n", var, input, output);
 	}
 
     input  = (float_t) 1000.0;
     for (int var = 5; var < 50; ++var) {
-        output = fltMaFilter(input,&my_ma);
+        output = fltIirFilter1(input,&my_iir);
         fprintf(fp,"%d,%f,%f\n", var, input, output);
 	}
 
     input  = (float_t) -1000.0;
     for (int var = 50; var < 100; ++var) {
-        output = fltMaFilter(input,&my_ma);
+        output = fltIirFilter1(input,&my_iir);
         fprintf(fp,"%d,%f,%f\n", var, input, output);
 	}
     fclose(fp);
@@ -47,5 +48,5 @@ void test_fltMaFilter(void)
 
 // not needed when using generate_test_runner.rb
 int main(void) {
-    test_fltMaFilter();
+    test_fltIirFilter1();
 }
--- src/EST_Module_Exercise/test/test_FltCtrl_fltMaFilter.c
+++ src/EST_Module_Exercise/test/test_FltCtrl_fltMaFilter.c
@@ -10,37 +10,36 @@ void tearDown(void) {
     // clean stuff up here
 }
 
-void test_fltIirFilter1(void)
+void test_fltMaFilter(void)
 {
-	FILE *fp = fopen(".\\test\\data\\FltIirFilter1Result.csv","w");
+	FILE *fp = fopen(".\\test\\data\\FltMaFilterResult.csv","w");
 
 	float_t input;
 	float_t output;
 
-	fltIir_t my_iir = FLT_IIR_FILTER1_DEFAULT;
+	fltMa_t my_ma = FLT_MA_FILTER_DEFAULT;
 
     // filter coefficients
-    my_iir.flt_b0           = (float_t)  0.1 ;
-    my_iir.flt_b1           = (float_t)  0.0 ;
-    my_iir.flt_a1           = (float_t) -0.9 ;
+    my_ma.flt_acc           = (float_t)  0.0 ;
+    my_ma.i16_length        = (int16_t)  16 ;
 
     fprintf(fp, "idx,  input,  output\n");
 
     input  = (float_t) 0.0;
     for (int var = 0; var < 5; ++var) {
-        output = fltIirFilter1(input,&my_iir);
+        output = fltMaFilter(input,&my_ma);
         fprintf(fp,"%d,%f,%f\n", var, input, output);
 	}
 
     input  = (float_t) 1000.0;
     for (int var = 5; var < 50; ++var) {
-        output = fltIirFilter1(input,&my_iir);
+        output = fltMaFilter(input,&my_ma);
         fprintf(fp,"%d,%f,%f\n", var, input, output);
 	}
 
     input  = (float_t) -1000.0;
     for (int var = 50; var < 100; ++var) {
-        output = fltIirFilter1(input,&my_iir);
+        output = fltMaFilter(input,&my_ma);
         fprintf(fp,"%d,%f,%f\n", var, input, output);
 	}
     fclose(fp);
@@ -48,5 +47,5 @@ void test_fltIirFilter1(void)
 
 // not needed when using generate_test_runner.rb
 int main(void) {
-    test_fltIirFilter1();
+    test_fltMaFilter();
 }
